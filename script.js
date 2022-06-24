e312142127b17b7a9594ee31e52331f1bc84a29b
yeralinaibat <yeralin4944@gmail.com>
2022-06-24 20:38:01 +0600

    add index.html, script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const numberOfFilms = prompt('skolko filmov vy posmotreli?', '');
+
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:{},
+    private:false
+};
+
+
+
+const a = prompt('Odin iz poslednih prosmotrennyh filmov', ''),
+      b = prompt('Na skolko ocenite ego?', ''),
+      c = prompt('Odin iz poslednih prosmotrennyh filmov', ''),
+      d = prompt('Na skolko ocenite ego?', '');
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB);
