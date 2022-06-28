22024b065d036086f94f78d786fed267d1b7ae85
yeralinaibat <yeralin4944@gmail.com>
2022-06-28 12:53:03 +0600

    q

--- script.js
+++ script.js
@@ -26,8 +26,8 @@ const personalMovieDB = {
 
 
 for (let i = 0; i < 2; i++){
-    const a = prompt('Odin iz poslednih prosmotrennyh filmov', ''),
-          b = prompt('Na skolko ocenite ego?', '');
+    const a = prompt('Odin iz poslednih prosmotrennyh filmov', '').trim(),
+          b = prompt('Na skolko ocenite ego?', '').trim();
 
     if (a!==null && b!==null && a!='' && b!='' && a.length < 50) {
         personalMovieDB.movies[a]=b;
@@ -156,6 +156,142 @@ function returnNeighboringNumbers(number) {
     return arr;
 }
 console.log(returnNeighboringNumbers(4)) */
+
+//v = a  
+
+/* function calculateVolumeAndArea(number) {
+
+    if (!Number.isInteger(number) || number<0 || typeof(number)!=='number') {
+        console.log('При вычислении произошла ошибка')
+    }else {
+        console.log('Объем куба:',number*number*number + "," + 'площадь всей поверхности: ' + 6*number*number );
+
+
+    }
+
+
+}
+calculateVolumeAndArea();
+
+
+if (!Number.isInteger(number) || number > 9||number=0 || typeof(number)!=='number'){
+
+
+function getCoupeNumber(number) {
+    if (number > 9|| number == 0 ){
+        console.log('Таких мест в вагоне не существует');
+    } else if (!Number.isInteger(number)|| typeof(number)!=='number'||number<0){
+        console.log('Ошибка. Проверьте правильность введенного номера');
+    }else{
+        let vagon = number/4;
+        console.log(Math.ceil(vagon));
+
+    }
+    
+
+}
+
+getCoupeNumber('hgj');
+
+function getTimeFromMinutes(number) {
+    if (number<0 ){
+        console.log('Ошибка, проверьте данные')        
+    }else{
+        let chas = number / 60;
+        minutes = number % 60;
+    console.log(`Это ${Math.floor(chas)} часа и ${Math.floor(minutes)} минут`);
+    }
+}
+getTimeFromMinutes (150);
+
+
+
+
+
+// Место для первой задачи
+function getTimeFromMinutes(number) {
+    if (number<0 ){
+        console.log('Ошибка, проверьте данные');
+        
+    }else {
+        
+        let chas = Math.floor(number / 60);
+        let min = Math.floor(number % 60);
+         let chasStr = '';
+
+    switch (chas) {
+        case 0: 
+            chasStr = 'часов';
+            break;
+        case 1:
+            chasStr = 'час';
+            break;
+        case 2:
+        case 3:
+        case 4:
+            chasStr = 'часа';
+            break;
+        default:
+            chasStr = 'часов';
+    }
+
+     console.log (`Это ${chas} ${chasStr} и ${min} минут`);
+    
+}
+}
+getTimeFromMinutes(-50);
+
+
+
+function findMaxNumber() {
+    if (typeof(a) !== 'number' ||
+        typeof(b) !== 'number' ||
+        typeof(c) !== 'number' ||
+        typeof(d) !== 'number') {
+        return 0;
+    } else {
+        return Math.max(a, b ,c, d);
+    }
+
+}
+console.log(findMaxNumber(1,2,4,7)); */
+
+
+
+/* function first(){
+
+    setTimeout(function(){
+        console.log(2);
+    } ,1000);
+}
+
+first(); */
+
+
+/* const options = {
+    name : 'test',
+    age: 28,
+    lastName: 'eser',
+    color: {
+        bg: 'black',
+        border: 'red',
+    }
+};
+
+
+/* for (let key in options) {
+    if (let key in options  ){
+        if (typeof options[key]) === 'object'){
+            console.log(`Svoistva ${key} imeet znachenie ${options[key][i]}`);
+        }
+    }
+    console.log(`Svoistva ${key} imeet znachenie ${options[key]}`);
+} */
+/* console.log (Object.keys(options).length); */
+
+
+
+
 
 
 
